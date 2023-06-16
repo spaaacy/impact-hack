a2474a0847a5cb0d8f57eb61d3ef0ff7c74b45c0
Aakif Ahamath <aakifahamath@gmail.com>
2023-06-16 17:42:53 +0800

    Setup packages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:impact_hack/main.dart';
+import 'package:impact_hack/ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
