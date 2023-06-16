82034e02c1d2d661fb019cc8efd50332a60ef680
Handika <handikaharianto01@gmail.com>
2023-06-16 22:58:06 +0800

    feat: add function to fetch business reviews

--- lib/services/business_service.dart
+++ lib/services/business_service.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart';
 
 import '../data/model/business_details.dart';
 import '../data/model/suggestion.dart';
+import '../data/model/review.dart';
 
 class BusinessService {
   final client = Client();
@@ -70,7 +71,45 @@ class BusinessService {
             aboutDetails: data['about']['details']);
       }
 
-      throw Exception("Business not found.");
+      throw Exception(result['error_message']);
+    } else {
+      throw Exception('Failed to fetch suggestion');
+    }
+  }
+
+  Future<List<Review>> fetchBusinessReviews(
+      {required String businessId,
+      int limit = 20,
+      required String lang}) async {
+    final request =
+        "https://local-business-data.p.rapidapi.com/business-reviews?business_id=$businessId&limit=$limit&region=ms&language=$lang";
+    final response = await client.get(Uri.parse(request), headers: {
+      'X-RapidAPI-Key': 'a368014d7emsh42b952724a1738dp199cbbjsnd23fb0d00942',
+      'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
+    });
+
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
+
+      if (result['status'] == 'OK') {
+        return result['data'].map<Review>((review) {
+          return Review(
+            reviewId: review["review_id"],
+            text: review["review_text"],
+            rating: review["rating"],
+            timestamp: review["timestamp"],
+            likeCount: review["like_count"],
+            ratingBreakdown: review["hotel_rating_breakdown"],
+            reviewForm: review["review_form"],
+          );
+        }).toList();
+      }
+
+      if (result['data'].length < 1) {
+        return [];
+      }
+
+      throw Exception(result['error_message']);
     } else {
       throw Exception('Failed to fetch suggestion');
     }
