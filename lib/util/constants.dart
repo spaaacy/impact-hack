5b3b4e3e9fa33852c6fbeda8992385db720b87c5
Aakif Ahamath <aakifahamath@gmail.com>
2023-06-21 15:06:58 +0800

    Added readme gifs

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,2 +1,2 @@
-const String rapidApiKey = "";
-const String openAiApiKey = "";
+const String rapidApiKey = "811032fe44mshd1dd2221a53a133p114d6fjsndebf3ff17ed0";
+const String openAiApiKey = "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";
